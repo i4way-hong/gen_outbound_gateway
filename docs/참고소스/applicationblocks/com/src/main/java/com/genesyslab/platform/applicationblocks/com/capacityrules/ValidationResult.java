e31378e05ac73b3b0557f46a1761143bb97a1111
jkhong <jkhong@i4way.co.kr>
2026-04-09 15:24:37 +0900

    SDK와 샘플 코드 삭제

--- docs/참고소스/applicationblocks/com/src/main/java/com/genesyslab/platform/applicationblocks/com/capacityrules/ValidationResult.java
+++
@@ -1,3 +0,0 @@
-package com.genesyslab.platform.applicationblocks.com.capacityrules;
-
-public enum ValidationResult { Unknown, Failed, Success };
--- docs/참고소스/applicationblocks/warmstandby/quickstart/quickstart.bat
+++
@@ -1,1 +0,0 @@
-java -jar target/warmstandbyappblockquickstart.jar
